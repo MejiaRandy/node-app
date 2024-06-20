27ed5e4b9bd7a05f0a152c6e21b110d4c246ed0a
MejiaRandy <randyamejia@hotmail.com>
2024-06-19 22:56:37 -0400

    Implement: NodeJS Mini App

---
+++ server.js
@@ -0,0 +1,27 @@
+// server.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Configurar motor de plantillas EJS
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+// Rutas
+const indexRouter = require('./routes/index');
+const valoresRouter = require('./routes/valores');
+const skillsRouter = require('./routes/skills');
+const proyectosRouter = require('./routes/proyectos');
+const contactoRouter = require('./routes/contacto');
+
+// Usar las rutas
+app.use('/', indexRouter);
+app.use('/valores', valoresRouter);
+app.use('/skills', skillsRouter);
+app.use('/proyectos', proyectosRouter);
+app.use('/contacto', contactoRouter);
+
+// Iniciar el servidor
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
